0b84e8da470e28094a523172a059398515c5405c
sohamsshah <sohamshah456@gmail.com>
2021-03-26 11:39:51 +0530

    got to change the library

--- src/components/Docs/Content/ButtonPage/ButtonPage.jsx
+++ src/components/Docs/Content/ButtonPage/ButtonPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import themes from "./../../Themes/Themes.module.css"
+import {Button} from "sodium-ui"
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 function ButtonPage() {
     return (
-        <div>
-            I am Button Page
-        </div>
+        <>
+          <h1 className={themes.heading1}>Buttons</h1> 
+          <p className={themes.lead}>Custom button styles for actions in forms, dialogs, and more in various variants, kinds and states.</p>
+        
+        <h2 className={themes.heading2}>Example</h2>
+        <Button variant="primary" kind="bordered">Default </Button>
+        </>
     )
 }
 
